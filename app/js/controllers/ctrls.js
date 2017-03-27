6e637cc8e588887cdb33afca9f3431764b575646
Achilles <achimoraites@yahoo.gr>
2017-03-27 13:03:08 +0300

    Api is working..

--- app/js/controllers/ctrls.js
+++ app/js/controllers/ctrls.js
@@ -1,10 +1,29 @@
 
 angular.module('app.controllers',[])
-.controller('MainCtrl', ['$scope',function($scope){
+.controller('MainCtrl', ['$scope','$http',function($scope,$http){
 
 $scope.a = 'It works!!!';
 $scope.search = 'Search';
-$scope.channels = true;
+$scope.channels = false;
+$scope.channelData = [];
 
+$scope.getChannel = function(searchTxt){
+  var apiUrl = 'http://wind-bow.glitch.me/twitch-api/channels/';
+  $http({
+        method: 'GET',
+        url: apiUrl + searchTxt,
+        params: {
+            format: 'json',
+            json_callback: 'callback'
+        }
+    }).success(function (data){
+    $scope.channelData = data;
+    $scope.channels = true;
+  }).error(function(data){
+        $scope.channelError = data;
+        $scope.channels = false;
+  });
+}
 }])
+
 ;
--- app/js/services/srv.js
+++ app/js/services/srv.js
@@ -1,5 +1,14 @@
 
 angular.module('app.services',[])
-.factory('twitchApi', function(){
+.factory('twitchApi', ['$http',function($http){
+var apiUrl = 'https://wind-bow.glitch.me/twitch-api';
 
-});
+
+return{
+  channel: function(searchTxt){
+  return  $http(apiUrl + '/channels/' + searchTxt ,{type:'GET'});
+  }
+};
+
+}])
+;
