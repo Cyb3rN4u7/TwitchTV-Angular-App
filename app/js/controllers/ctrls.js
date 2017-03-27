7d5e858b749e68b1cf6c94b5b974d7e3be3d1d84
Achilles <achimoraites@yahoo.gr>
2017-03-27 19:46:21 +0300

    Added channels

--- app/js/controllers/ctrls.js
+++ app/js/controllers/ctrls.js
@@ -5,8 +5,9 @@ angular.module('app.controllers',[])
 $scope.a = 'It works!!!';
 $scope.search = 'Search';
 $scope.channels = false;
+$scope.isOnline = true;
 $scope.channelData = [];
-
+$scope.channelsList = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
 $scope.getChannel = function(searchTxt){
   var apiUrl = 'http://wind-bow.glitch.me/twitch-api/channels/';
   $http({
@@ -17,13 +18,20 @@ $scope.getChannel = function(searchTxt){
             json_callback: 'callback'
         }
     }).success(function (data){
-    $scope.channelData = data;
+    $scope.channelData.push(data);
     $scope.channels = true;
   }).error(function(data){
         $scope.channelError = data;
         $scope.channels = false;
   });
 }
+//$scope.getChannel('freecodecamp');
+for (channel in $scope.channelsList){
+  //alert($scope.channelsList[channel]);
+  $scope.getChannel($scope.channelsList[channel]);
+}
+
+
 }])
 
 ;
