2bcc902af6f24dfb0f2bf116fd2cdd865a8c5d78
Cyb3rN4u7 <achimoraites@yahoo.gr>
2018-07-17 13:27:59 +0300

    added error handling when a channel is not found

--- app/js/controllers/ctrls.js
+++ app/js/controllers/ctrls.js
@@ -7,16 +7,21 @@ $scope.search = 'Search';
 $scope.channels = false;
 $scope.searched = false;
 $scope.isOnline = true;
+$scope.showChannel = false;
 $scope.channelData = [];
 $scope.channelsList = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp","LeagueofLegends", "habathcx", "RobotCaleb", "noobs2ninjas"];
 $scope.searchedList =[];
 $scope.getChannels = function(searchTxt){
   twitchApi.channel(searchTxt).success(function (data){
+    
     $scope.channelData.push(data);
     $scope.channels = true;
+
+ 
   }).error(function(data){
       $scope.channelData.unshift(data);
         $scope.channels = false;
+
   });
 }
 
@@ -28,9 +33,15 @@ $scope.searchChannel = function(searchTxt){
     $scope.searchedChannelData = data ;
     $scope.searchedList.push(searchTxt);
     $scope.searched = true;
+    $scope.showChannel = true;
+
+    if(data.hasOwnProperty('error')){
+      var title = "not found: "+ searchTxt;
+      $scope.searchedChannelData = {url:"#", logo:"./img/notfound.jpg",display_name : title, status:"Staring at the unknown",followers: "You", updated_at: "2018-07-15T11:00:43Z",views:"0"};
+    }
   }).error(function(data){
-      alert(data.message);
-      $scope.searchedChannelData = data;
+      $scope.searchedChannelData = {url:"#", logo:"./img/notfound.jpg",display_name : "Errors Gone bad", status:"0",followers: "0", updated_at: "2018-07-15T11:00:43Z",views:"0"};
+      $scope.showChannel = false;
   });
 }
 
